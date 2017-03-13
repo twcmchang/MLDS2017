d0afc1d899efef3a26d5b54d72182fa89cca2696
cmchang <chunming00@icloud.com>
2017-03-13 12:15:58 +0800

    update main.py

--- hw1/main.py
+++ hw1/main.py
@@ -26,6 +26,8 @@ initial = False
 
 data_path = "/Users/chunmingchang/MLDS2017/hw1/Data"
 
+print("Initial:",str(initial))
+
 if initial:
     # build dictionary
     myDict = Dict(train_path= os.path.join(data_path,'train_sentence'),
@@ -53,6 +55,8 @@ else:
     myDict.GetWordIndex('test')
     selected_sentences = pickle.load(open(os.path.join(data_path,'pickle_sentence_0.80_10_00862361'),'rb'))
 
+print("Completed dictionary and selected_sentences ...")
+
 # function: generate one hot embedding matrix
 def dense_to_one_hot(labels_dense, num_classes=10):
     num_labels = labels_dense.shape[0]
@@ -69,8 +73,8 @@ embedding_matrix = dense_to_one_hot(np.array(range(len(myDict.both_dict))),num_c
 batch_size     = 128
 window         = 4 # how many forewords to determine the next words
 learning_rate  = 0.001
-training_iters = 100000 # upper bound of epoch * batch_size
-display_epoch  = 10 # display per display_epoch 
+training_iters = 5000000 # upper bound of epoch * batch_size
+display_epoch  = 50 # display per display_epoch 
 
 # Network Parameters
 n_input   = len(myDict.both_dict) # because of one-hot encoding here
@@ -137,10 +141,16 @@ accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 # Initializing the variables
 init = tf.global_variables_initializer()
 
+#
+saver = tf.train.Saver()
+
+print("Starting training RNN ...")
+
 # Launch the graph
 with tf.Session() as sess:
     sess.run(init)
     step = 1
+    bestaccu = 0.0
     # Keep training until reach max iterations
     while step * batch_size < training_iters:
         # generate a batch of samples
@@ -153,15 +163,22 @@ with tf.Session() as sess:
 
         # Run optimization op (backprop)
         sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-        if step % display_step == 0:
-            # Calculate batch accuracy
-            acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
-            # Calculate batch loss
-            loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+
+        # Calculate batch accuracy
+        acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+        # Calculate batch loss
+        loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+
+        if step % display_epoch == 0:
             print(  "Iter " + str(step*batch_size) +
                     ", Minibatch Loss= "    + "{:.6f}".format(loss) +
                     ", Training Accuracy= " + "{:.5f}".format(acc))
         step += 1
+        if acc > bestaccu:
+            bestaccu = acc
+	    # Save the variables to disk.
+            save_path = saver.save(sess, os.path.join(data_path,"model"+str(acc)+".ckpt"))
+            print("Model saved in file: %s" % save_path)
     print("Optimization Finished!")
 
 #     # Calculate accuracy for 128 mnist test images
