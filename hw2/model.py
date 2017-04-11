90e9d6049eba69c2f1613f9a00f64a5fac7757fe
cmchang <chunming00@icloud.com>
2017-04-12 01:31:30 +0800

    using args to assign model parameters instead

--- hw2/model.py
+++ hw2/model.py
@@ -2,139 +2,164 @@ import tensorflow as tf
 import numpy as np
 
 class Video_Caption_Generator():
-	def __init__(self, dim_image, n_video_step, n_caption_step, dim_hidden, n_lstm_step, batch_size, n_vocab, learning_rate, grad_clip):
+	def __init__(self, args, n_vocab, infer):
 		# model parameters
-		self.dim_image 		= dim_image
-		self.n_video_step 	= n_video_step
-		self.n_caption_step = n_caption_step
-		self.dim_hidden 	= dim_hidden
-		self.n_lstm_step 	= n_lstm_step
-		self.batch_size 	= batch_size
+		self.dim_image 		= args.dim_image
+		self.n_video_step 	= args.n_video_step
+		self.n_caption_step = args.n_caption_step
+		self.dim_hidden 	= args.dim_hidden
+		self.n_lstm_step 	= args.n_lstm_step
+		self.batch_size 	= args.batch_size
+		self.learning_rate	= args.learning_rate
+		self.grad_clip		= args.grad_clip
 		self.n_vocab 		= n_vocab
-		self.learning_rate	= learning_rate
-		self.grad_clip		= grad_clip
 
 		# model components
 		# two LSTM layers
 		# word embedding for input
 		with tf.device("/cpu:0"):
-			self.Wemb = tf.Variable(tf.random_uniform([n_vocab, dim_hidden],-0.1,0.1), name="Wemb")
-		# with tf.device("/cpu:0"):
-		# 	self.Wemb = tf.get_variable("Wemb",tf.random_uniform([self.n_vocab, self.dim_hidden],-0.1,0.1))
-		
-		#with tf.variable_scope("model"):
-		#with tf.variable_scope("LSTM1"):
-		self.lstm1 = tf.contrib.rnn.BasicLSTMCell(dim_hidden, state_is_tuple=False)
-		#with tf.variable_scope("LSTM2"):
-		self.lstm2 = tf.contrib.rnn.BasicLSTMCell(dim_hidden, state_is_tuple=False)
+			self.Wemb = tf.Variable(tf.random_uniform([self.n_vocab, self.dim_hidden],-0.1,0.1), name="Wemb")
+	
+		self.lstm1 = tf.contrib.rnn.BasicLSTMCell(self.dim_hidden, state_is_tuple=False)
+		self.lstm2 = tf.contrib.rnn.BasicLSTMCell(self.dim_hidden, state_is_tuple=False)
 
 		# image embedding
-		self.embed_image_W = tf.Variable(tf.random_uniform([dim_image, dim_hidden],-0.1,0.1), name="embed_image_W")
-		self.embed_image_b = tf.Variable(tf.zeros([dim_hidden]), name="embed_image_b")
-		# self.embed_image_W = tf.get_variable("embed_image_W",tf.random_uniform([dim_image, dim_hidden],-0.1,0.1))
-		# self.embed_image_b = tf.get_variable("embed_image_b",tf.zeros([dim_hidden]))
-
+		self.embed_image_W = tf.Variable(tf.random_uniform([self.dim_image, self.dim_hidden],-0.1,0.1), name="embed_image_W")
+		self.embed_image_b = tf.Variable(tf.zeros([self.dim_hidden]), name="embed_image_b")
 
 		# word embedding for output
-		self.embed_word_W  = tf.Variable(tf.random_uniform([dim_hidden,n_vocab],-0.1,0.1), name="embed_word_W")
-		self.embed_word_b  = tf.Variable(tf.zeros([n_vocab]), name="embed_word_b")
-		# self.embed_word_W  = tf.get_variable("embed_word_W",tf.random_uniform([dim_hidden,n_vocab],-0.1,0.1))
-		# self.embed_word_b  = tf.get_variable("embed_word_b",tf.zeros([n_vocab]))
+		self.embed_word_W  = tf.Variable(tf.random_uniform([self.dim_hidden,self.n_vocab],-0.1,0.1), name="embed_word_W")
+		self.embed_word_b  = tf.Variable(tf.zeros([self.n_vocab]), name="embed_word_b")
 
-		# def build_model(self):
-		# 	# input
-		self.video = tf.placeholder(tf.float32, [self.batch_size, self.n_video_step, self.dim_image])
-		self.video_mask = tf.placeholder(tf.float32, [self.batch_size, self.n_video_step])
+		if infer == False:
+			# input
+			self.video = tf.placeholder(tf.float32, [self.batch_size, self.n_video_step, self.dim_image])
+			self.video_mask = tf.placeholder(tf.float32, [self.batch_size, self.n_video_step])
 
-		# output
-		self.caption = tf.placeholder(tf.int32, [self.batch_size, self.n_caption_step+1]) # int32 for embedding_look_up
-		self.caption_mask = tf.placeholder(tf.float32, [self.batch_size, self.n_caption_step+1])
+			# output
+			self.caption = tf.placeholder(tf.int32, [self.batch_size, self.n_caption_step+1]) # int32 for embedding_look_up
+			self.caption_mask = tf.placeholder(tf.float32, [self.batch_size, self.n_caption_step+1])
 
-		# results
-		pred_probs = []
-		loss = 0.0
+			# results
+			pred_probs = []
+			loss = 0.0
 
-		### Step1: transform video into embedded video ###
-		video_flat = tf.reshape(self.video, [-1, self.dim_image]) # (batch_size*n_video_step, dim_image)
-		image_embed = tf.nn.xw_plus_b(video_flat, self.embed_image_W, self.embed_image_b)
-		image_embed = tf.reshape(image_embed, [self.batch_size, self.n_video_step, self.dim_hidden])
+			### Step1: transform video into embedded video ###
+			video_flat = tf.reshape(self.video, [-1, self.dim_image]) # (batch_size*n_video_step, dim_image)
+			image_embed = tf.nn.xw_plus_b(video_flat, self.embed_image_W, self.embed_image_b)
+			image_embed = tf.reshape(image_embed, [self.batch_size, self.n_video_step, self.dim_hidden])
 
-		### Step2: (encoding stage) embedded images into LSTM ###
-		# initialization states in 2 LSTM layers
-		state1 = tf.zeros([self.batch_size, self.lstm1.state_size])
-		state2 = tf.zeros([self.batch_size, self.lstm2.state_size])
+			### Step2: (encoding stage) embedded images into LSTM ###
+			# initialization states in 2 LSTM layers
+			state1 = tf.zeros([self.batch_size, self.lstm1.state_size])
+			state2 = tf.zeros([self.batch_size, self.lstm2.state_size])
 
-		# padding when entering LSTM2
-		# A word is embedded as a vector of dim_hidden by Wemb, so the padding vector is with dim_hidden
-		padding = tf.zeros([self.batch_size, self.dim_hidden]) 
+			# padding when entering LSTM2
+			# A word is embedded as a vector of dim_hidden by Wemb, so the padding vector is with dim_hidden
+			padding = tf.zeros([self.batch_size, self.dim_hidden]) 
 
-		# with tf.variable_scope("model",reuse=True):
-		for i in range(0,self.n_video_step):
-		# if i>0:
-		# 	tf.get_variable_scope().reuse_variables()
-		# with tf.variable_scope(name_or_scope="LSTM1",reuse=True):
-			if i == 0:
-				with tf.variable_scope("LSTM1"):
-					output1, state1 = self.lstm1(image_embed[:,i,:],state1)
-				with tf.variable_scope("LSTM2"):
-					output2, state2 = self.lstm2(tf.concat([padding, output1],1), state2)
-			else:
+			for i in range(0,self.n_video_step):
+				if i == 0:
+					with tf.variable_scope("LSTM1"):
+						output1, state1 = self.lstm1(image_embed[:,i,:],state1)
+					with tf.variable_scope("LSTM2"):
+						output2, state2 = self.lstm2(tf.concat([padding, output1],1), state2)
+				else:
+					with tf.variable_scope("LSTM1",reuse=True):
+						output1, state1 = self.lstm1(image_embed[:,i,:],state1)
+					with tf.variable_scope("LSTM2",reuse=True):
+						output2, state2 = self.lstm2(tf.concat([padding, output1],1), state2)
+
+			### Step 3: (decoding stage) acquire prediction from LSTM2 output ###
+
+			for i in range(0, self.n_caption_step):
+				with tf.device("/cpu:0"):
+					current_word_embed = tf.nn.embedding_lookup(self.Wemb, self.caption[:,i])
+
 				with tf.variable_scope("LSTM1",reuse=True):
-					output1, state1 = self.lstm1(image_embed[:,i,:],state1)
+					output1, state1 = self.lstm1(padding, state1)
 				with tf.variable_scope("LSTM2",reuse=True):
-					output2, state2 = self.lstm2(tf.concat([padding, output1],1), state2)
+					output2, state2 = self.lstm2(tf.concat([current_word_embed,output1],1),state2)
 
-		### Step 3: (decoding stage) acquire prediction from LSTM2 output ###
+				### Step 4: calculate loss ### 
+				# create answer 
+				answer_index_in_vocab = self.caption[:,i+1]
+				answer_index_in_vocab = tf.expand_dims(answer_index_in_vocab, 1)
+				batch_index	= tf.expand_dims(tf.range(0, self.batch_size, 1), 1)
+				sparse_mat 	= tf.concat([batch_index, answer_index_in_vocab], 1)
+				answer_in_onehot = tf.sparse_to_dense(sparse_mat, np.asarray([self.batch_size, self.n_vocab]), 1.0, 0.0)
 
-		for i in range(0, self.n_caption_step):
-			#with tf.variable_scope('model'):
-			#tf.get_variable_scope().reuse_variables()
-			with tf.device("/cpu:0"):
-				current_word_embed = tf.nn.embedding_lookup(self.Wemb, self.caption[:,i])
-			# if i == 0:
-			# 	with tf.variable_scope("LSTM1"):
-			# 		output1, state1 = self.lstm1(padding, state1)
-			# 	with tf.variable_scope("LSTM2"):
-			# 		output2, state2 = self.lstm2(tf.concat([current_word_embed,output1],1),state2)
-			# else:
-			with tf.variable_scope("LSTM1",reuse=True):
-				output1, state1 = self.lstm1(padding, state1)
-			with tf.variable_scope("LSTM2",reuse=True):
-				output2, state2 = self.lstm2(tf.concat([current_word_embed,output1],1),state2)
+				# acquire output
+				logits = tf.nn.xw_plus_b(output2, self.embed_word_W, self.embed_word_b)
+				cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=answer_in_onehot)
+				cross_entropy = cross_entropy * self.caption_mask[:,i]
 
-			### Step 4: calculate loss ### 
-			# with tf.variable_scope("model"):
-			# 	# create answer 
-			answer_index_in_vocab = self.caption[:,i+1]
-			answer_index_in_vocab = tf.expand_dims(answer_index_in_vocab, 1)
-			batch_index	= tf.expand_dims(tf.range(0, self.batch_size, 1), 1)
-			sparse_mat 	= tf.concat([batch_index, answer_index_in_vocab], 1)
-			answer_in_onehot = tf.sparse_to_dense(sparse_mat, np.asarray([self.batch_size, self.n_vocab]), 1.0, 0.0)
+				pred_probs.append(logits)
 
-			# acquire output
-			logits = tf.nn.xw_plus_b(output2, self.embed_word_W, self.embed_word_b)
-			cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=answer_in_onehot)
-			cross_entropy = cross_entropy * self.caption_mask[:,i]
+				this_loss = tf.reduce_sum(cross_entropy)/self.batch_size
+				loss = loss + this_loss
 
-			# 
-			pred_probs.append(logits)
+			self.tf_loss = loss
+			self.tf_probs = pred_probs
 
-			this_loss = tf.reduce_sum(cross_entropy)/self.batch_size
-			loss = loss + this_loss
+			tvars = tf.trainable_variables()
+			grads, _ = tf.clip_by_global_norm(tf.gradients(self.tf_loss, tvars), clip_norm = self.grad_clip)
+			optimizer = tf.train.AdamOptimizer(self.learning_rate)
+			self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+			# self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.tf_loss)
 
-		self.tf_loss = loss
-		self.tf_probs = pred_probs
+		else: # infer == True
+			self.video = tf.placeholder(tf.float32, [1, self.n_video_lstm_step, self.dim_image])
+			self.video_mask = tf.placeholder(tf.float32, [1, self.n_video_lstm_step])
 
-		# tvars = tf.trainable_variables()
-		# grads, _ = tf.clip_by_global_norm(tf.gradients(self.tf_loss, tvars), clip_norm = self.grad_clip)
-		# optimizer = tf.train.AdamOptimizer(self.learning_rate)
-		# self.train_op = optimizer.apply_gradients(zip(grads, tvars))
-		self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.tf_loss)
+			video_flat = tf.reshape(video, [-1, self.dim_image])
+			image_emb = tf.nn.xw_plus_b(video_flat, self.encode_image_W, self.encode_image_b)
+			image_emb = tf.reshape(image_emb, [1, self.n_video_lstm_step, self.dim_hidden])
 
-	# def get_generator():
-	# 	return True
+			state1 = tf.zeros([1, self.lstm1.state_size])
+			state2 = tf.zeros([1, self.lstm2.state_size])
+			padding = tf.zeros([1, self.dim_hidden])
 
+			gen_caption_idx = []
 
+			pred_probs = []
+			embeds = []
 
+			for i in range(0,self.n_video_step):
+				if i == 0:
+					with tf.variable_scope("LSTM1"):
+						output1, state1 = self.lstm1(image_embed[:,i,:],state1)
+					with tf.variable_scope("LSTM2"):
+						output2, state2 = self.lstm2(tf.concat([padding, output1],1), state2)
+				else:
+					with tf.variable_scope("LSTM1",reuse=True):
+						output1, state1 = self.lstm1(image_embed[:,i,:],state1)
+					with tf.variable_scope("LSTM2",reuse=True):
+						output2, state2 = self.lstm2(tf.concat([padding, output1],1), state2)
 
+			for i in range(0, self.n_caption_step):
 
+				if i == 0:
+					# For testing, here the first word of caption must be <BOS>, 
+					# <BOS> is defined at the index of 0 in function build_word_vocabuary in utils.py.	
+					with tf.device("/cpu:0"):
+						current_word_embed = tf.nn.embedding_lookup(self.Wemb, tf.zeros([0],dtype=tf.int32))
+
+				with tf.variable_scope("LSTM1",reuse=True):
+					output1, state1 = self.lstm1(padding, state1)
+				with tf.variable_scope("LSTM2",reuse=True):
+					output2, state2 = self.lstm2(tf.concat([current_word_embed,output1],1),state2)
+
+				logits = tf.nn.xw_plus_b( output2, self.embed_word_W, self.embed_word_b)
+				max_prob_index = tf.argmax(logits, 1)[0]
+				gen_caption_idx.append(max_prob_index)
+				pred_probs.append(logits)
+
+				# update current_word_embed
+				with tf.device("/cpu:0"):
+					current_word_embed = tf.nn.embedding_lookup(self.Wemb, max_prob_index)
+					# current_word_embed = tf.expand_dims(current_word_embed, 0)
+				embeds.append(current_embed)
+
+			self.gen_caption_idx = gen_caption_idx
+			self.pred_probs = pred_probs
--- hw2/train.py
+++ hw2/train.py
@@ -9,90 +9,93 @@ from utils import data_preprocess, get_video_feat, get_padding_caption
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--data_dir', type=str, default='data/Holmes_Training_Data',
-	help='data directory containing input.txt')
 	parser.add_argument('--train_video_feat_path', type=str, default='MLDS_hw2_data/training_data/feat/',
-	help='directory to store checkpointed models')
+						help='directory to store checkpointed models')
 	parser.add_argument('--test_video_feat_path', type=str, default='MLDS_hw2_data/testing_data/feat/',
-	help='directory to store checkpointed models')
+						help='directory to store checkpointed models')
 	parser.add_argument('--train_label_json', type=str, default='MLDS_hw2_data/training_label.json',
-	help='directory to store checkpointed models')
+						help='directory to store checkpointed models')
 	parser.add_argument('--test_label_json', type=str, default='MLDS_hw2_data/testing_public_label.json',
-	help='directory to store checkpointed models')
+						help='directory to store checkpointed models')
 	parser.add_argument('--save_dir', type=str, default='save',
-	help='directory to store checkpointed models')
+						help='directory to store checkpointed models')
 	parser.add_argument('--dim_image', type=int, default=4096,
-	help='dimension of input image')
+						help='dimension of input image')
 	parser.add_argument('--dim_hidden', type=int, default=1000,
-	help='dimension of LSTM hidden state')
+						help='dimension of LSTM hidden state')
 	parser.add_argument('--n_lstm_step', type=int, default=80,
-	help='number of LSTM steps')
+						help='number of LSTM steps')
 	parser.add_argument('--n_video_step', type=int, default=80,
-	help='number of video steps')
+						help='number of video steps')
 	parser.add_argument('--n_caption_step', type=int, default=20,
-	help='number of caption steps')
+						help='number of caption steps')
 	parser.add_argument('--n_epoch', type=int, default=1000,
-	help='number of epochs')
+						help='number of epochs')
 	parser.add_argument('--batch_size', type=int, default=50,
-	help='minibatch size')
+						help='minibatch size')
 	parser.add_argument('--save_every', type=int, default=10,
-	help='save frequency')
+						help='save frequency')
 	parser.add_argument('--learning_rate', type=float, default=0.001,
-	help='learning rate')
+						help='learning rate')
 	parser.add_argument('--grad_clip', type=float, default=10.,
-	help='clip gradients at this value')
+						help='clip gradients at this value')
 	parser.add_argument('--gpu_mem', type=float, default=0.800,
-	help='% of gpu memory to be allocated to this process. Default is 80%')
-	# parser.add_argument('--log_dir', type=str, default='logs',
-	# help='directory containing tensorboard logs')
-	# parser.add_argument('--init_from', type=str, default=None,
-	# help="""continue training from saved model at this path. Path must contain files saved by previous training process:
-	#     'config.pkl'        : configuration;
-	#     'words_vocab.pkl'   : vocabulary definitions;
-	#     'checkpoint'        : paths to model file(s) (created by tf).
-	#                           Note: this file contains absolute paths, be careful when moving files around;
-	#     'model.ckpt-*'      : file(s) with model definition (created by tf)
-	# """)
+						help='% of gpu memory to be allocated to this process. Default is 80%')
+	parser.add_argument('--log_dir', type=str, default='log',
+						help='directory containing log text')
+	parser.add_argument('--init_from', type=str, default=None,
+						help="""continue training from saved model at this path. Path must contain files saved by previous training process:
+						    'config.pkl'        : configuration;
+						    'checkpoint'        : paths to model file(s) (created by tf).
+						                          Note: this file contains absolute paths, be careful when moving files around;
+						    'model.ckpt-*'      : file(s) with model definition (created by tf)
+						""")
 
 	args = parser.parse_args()
 	train(args)
 
 def train(args):
+	if args.init_from is not None:
+		# check if all necessary files exist
+		assert os.path.isfile(os.path.join(args.init_from,"config.pkl")), "config.pkl file does not exist in path %s" % args.init_from
+		
+		ckpt = tf.train.get_checkpoint_state(args.init_from)
+		
+		# open old config and check if models are compatible
+		with open(os.path.join(args.init_from, 'config.pkl'), 'rb') as f:
+			saved_args = cPickle.load(f)
+		need_be_same=["model","dim_image","dim_hidden","n_lstm_step","n_video_step","n_caption_step"]
+		for checkme in need_be_same:
+			assert vars(saved_args)[checkme]==vars(args)[checkme],"Command line argument and saved model disagree on '%s' "%checkme
+	
+	with open(os.path.join(args.save_dir, 'config.pkl'), 'wb') as f:
+		cPickle.dump(args, f)
+
 	vocab, vocab_inv, train_feat_id, train_caption, test_feat_id, test_caption = data_preprocess(args.train_label_json,args.test_label_json)
 
-	model = Video_Caption_Generator(
-			dim_image = args.dim_image,
-			dim_hidden = args.dim_hidden,
-			batch_size = args.batch_size,
-			n_video_step = args.n_video_step,
-			n_caption_step = args.n_caption_step,
-			n_lstm_step	= args.n_lstm_step,
-			n_vocab = len(vocab),
-			learning_rate = args.learning_rate,
-			grad_clip = args.grad_clip)
-
-	init = tf.global_variables_initializer()
+	model = Video_Caption_Generator(args,n_vocab=len(vocab),infer=False)
 	
 	with tf.Session() as sess:
-		init.run()
-		print("Initialized")
-		# train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(tf_loss)
-		saver = tf.train.Saver(max_to_keep=100, write_version=1)
-		tf.global_variables_initializer().run()
 
-		loss_fd = open('loss.txt', 'w')
+		tf.global_variables_initializer().run()
+		print("Initialized")
+		
+		saver = tf.train.Saver(tf.global_variables())
+		if args.init_from is not None:
+			saver.restore(sess, ckpt.model_checkpoint_path)
+
+
+		loss_fd = open('log/loss.txt', 'w')
 		loss_to_draw = []
 
 		for epoch in range(0, args.n_epoch):
-			loss_to_draw_epoch = []
-
+			# shuffle 
 			index = np.array(range(len(train_feat_id)))
 			np.random.shuffle(index)
 			epoch_train_feat_id	 = train_feat_id[index]
 			epoch_train_caption = train_caption[index]
 
-			# current_train_data = train_data.groupby('video_path').apply(lambda x: x.irow(np.random.choice(len(x))))
-			# current_train_data = current_train_data.reset_index(drop=True)
+			loss_to_draw_epoch = []
 
 			for start, end in zip(
 					range(0, len(epoch_train_feat_id), model.batch_size),
@@ -100,40 +103,34 @@ def train(args):
 
 				start_time = time.time()
 
-				batch_video_id = epoch_train_feat_id[start:end]
+				# get one minibatch
+				batch_feat_id = epoch_train_feat_id[start:end]
 				batch_caption = epoch_train_caption[start:end]
 				
 				# get vdieo features
-				current_video_feat, current_video_feat_mask = get_video_feat(args.train_video_feat_path, batch_video_id)
+				current_feat, current_feat_mask = get_video_feat(args.train_video_feat_path, batch_feat_id)
 
 				# randomly select one captions for one video and get padding captions with maxlen = 20
 				current_caption, current_caption_mask = get_padding_caption(vocab, batch_caption, maxlen= model.n_caption_step+1)
 
-				_, loss_val = sess.run([model.train_op, model.tf_loss],feed_dict={
-							model.video: current_video_feat,
-							model.video_mask : current_video_feat_mask,
+				# run train_op to optimizer tf_loss
+				_, loss_val = sess.run([model.gen_caption_idx, model.pred_prob],feed_dict={
+							model.video: current_feat,
+							model.video_mask : current_feat_mask,
 							model.caption: current_caption,
 							model.caption_mask: current_caption_mask
 							})
+
 				loss_to_draw_epoch.append(loss_val)
 
 				print('idx: ', start, " Epoch: ", epoch, " loss: ", loss_val, ' Elapsed time: ', str((time.time() - start_time)))
 				loss_fd.write('epoch ' + str(epoch) + ' loss ' + str(loss_val) + '\n')
-
-			# # draw loss curve every epoch
-			# loss_to_draw.append(np.mean(loss_to_draw_epoch))
-			# plt_save_dir = "./loss_imgs"
-			# plt_save_img_name = str(epoch) + '.png'
-			# plt.plot(range(len(loss_to_draw)), loss_to_draw, color='g')
-			# plt.grid(True)
-			# plt.savefig(os.path.join(plt_save_dir, plt_save_img_name))
-
 			if np.mod(epoch, args.save_every) == 0:
-				print("Epoch ", epoch, " is done. Saving the model ...")
-				saver.save(sess, os.path.join(args.save_dir, 'model'), global_step=epoch)
+				checkpoint_path = os.path.join(args.save_dir, 'model.ckpt')
+				saver.save(sess, checkpoint_path, global_step=epoch)
+				print("Epoch ", epoch, "model saved to {}".format(checkpoint_path))
 		loss_fd.close()
 
-# def test():
 if __name__ == '__main__':
     main()
 
