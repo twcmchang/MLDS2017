84d56e3abe256aed59a39f1ad57e50843f8b1beb
cmchang <chunming00@icloud.com>
2017-04-27 23:27:34 +0800

    usage: bash run.sh [testing_data_path] [output_file]

--- hw2/run.sh
+++ hw2/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget "https://www.dropbox.com/s/pb7zjd7xy9cjyfq/model.zip?dl=0" -O model.zip
 unzip model.zip -d save/ 
-python test.py --testing_file "$1" --testing_path "$2" --result_file "$3"
+python3 test.py --testing_file "$1" --testing_path "$2" --result_file "$3"
