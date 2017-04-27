cb87611c73f507e26bda3731eb627600c436f97b
cmchang <chunming00@icloud.com>
2017-04-27 23:56:30 +0800

    bash run.sh [testing_id_txt] [testing_feat_path] [output_file]

--- hw2/run.sh
+++ hw2/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget "https://www.dropbox.com/s/pb7zjd7xy9cjyfq/model.zip?dl=0" -O model.zip
+wget -O model.zip "https://www.dropbox.com/s/pb7zjd7xy9cjyfq/model.zip?dl=0"
 unzip model.zip -d save/ 
 python3 test.py --testing_file "$1" --testing_path "$2" --result_file "$3"
