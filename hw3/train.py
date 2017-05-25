e8411927abf69b4893a0bbf9ce8ccb8d1704a3e2
gtx980 <d05921018@ntu.edu.tw>
2017-05-25 17:23:12 +0800

    Add infer=False in model.py and generate.py, and add y_dim=9600 experiment

--- hw3/train.py
+++ hw3/train.py
@@ -10,7 +10,7 @@ from six.moves import cPickle
 
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.per_process_gpu_memory_fraction = 0.3
 set_session(tf.Session(config=config))
 
 from model import DCGAN, WGAN, WGAN_v2
