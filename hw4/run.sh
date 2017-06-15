6d92501cc6b9f952edc8127ce1ae9025964a782f
mikechung99 <b99505003@ntu.edu.tw>
2017-06-15 23:43:42 +0800

    Create run.sh

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -O works.zip "https://www.dropbox.com/s/v0dtiuchgtgnmkx/works.zip?dl=0"
-unzip works.zip -d ./
+unzip -u works.zip
 python3 main.py --mode test --model_name "$1" --test_dataset_path "$2" --results_dir "$3"
