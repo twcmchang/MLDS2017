58d7f017ada59412c6c6012481f8893643b9912d
mikechung99 <b99505003@ntu.edu.tw>
2017-06-15 23:24:36 +0800

    Add files via upload

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O works.zip "https://www.dropbox.com/s/xplxts5g9ousjs6/model.zip?dl=0"
+wget -O works.zip "https://www.dropbox.com/s/v0dtiuchgtgnmkx/works.zip?dl=0"
 unzip works.zip -d ./
 python3 main.py --mode test --model_name "$1" --test_dataset_path "$2" --results_dir "$3"
